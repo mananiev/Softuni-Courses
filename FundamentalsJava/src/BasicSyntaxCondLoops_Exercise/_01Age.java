8c5cd7361f9956be9c1ef2f8d1957cf5b3c3bc7c
mananiev <m.a.ananiev@gmail.com>
2022-06-05 19:01:05 +0300

    Commit

---
+++ FundamentalsJava/src/BasicSyntaxCondLoops_Exercise/_01Age.java
@@ -0,0 +1,11 @@
+package BasicSyntaxCondLoops_Exercise;
+
+import java.util.Scanner;
+
+public class _01Age {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }}
