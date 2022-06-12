fe6d9c86c04874db38c1be4410134f8257187835
mananiev <m.a.ananiev@gmail.com>
2022-06-12 18:34:08 +0300

    Commit of the day

--- FundamentalsJava/src/Arrays/_03_SumEvenNumbers.java
+++ FundamentalsJava/src/Arrays/_03_SumEvenNumbers.java
@@ -1,9 +1,31 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _03_SumEvenNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String inputLine = scanner.nextLine();
+        int sum = 0;
+
+        int[] numArr = Arrays
+                .stream(inputLine.split( " "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i = 0; i < numArr.length; i++) {
+
+            if (numArr[i] %2 == 0) {
+
+                sum += numArr[i];
+            }
+
+
+        }
+        System.out.println(sum);
     }
+
+
 }
--- FundamentalsJava/src/Arrays/_04_ReverseAnArrayOfStrings.java
+++ FundamentalsJava/src/Arrays/_04_ReverseAnArrayOfStrings.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class _04_ReverseAnArrayOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        
     }
 }
