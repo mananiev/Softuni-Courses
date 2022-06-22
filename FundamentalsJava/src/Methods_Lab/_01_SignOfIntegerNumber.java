1d58f5fc76ee1b4b2ab31c3b9ca73ae245c4b36c
mananiev <m.a.ananiev@gmail.com>
2022-06-22 13:33:45 +0300

    Methods Lab start

--- FundamentalsJava/src/Methods_Lab/_01_SignOfIntegerNumber.java
+++ FundamentalsJava/src/Methods_Lab/_01_SignOfIntegerNumber.java
@@ -6,6 +6,19 @@ public class _01_SignOfIntegerNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input = Integer.parseInt(scanner.nextLine());
+
+        printSign(input);
         
+    }
+
+    public static void printSign(int num){
+        if (num>0){
+            System.out.printf("The number %d is positive.", num);
+        } else if (num< 0){
+            System.out.printf("The number %d is negative.", num);
+        } else {
+            System.out.println("The number 0 is zero.");
+        }
     }
 }
--- FundamentalsJava/src/Methods_Lab/_02_Grades.java
+++ FundamentalsJava/src/Methods_Lab/_02_Grades.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class _02_Grades {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+
     }
 }
