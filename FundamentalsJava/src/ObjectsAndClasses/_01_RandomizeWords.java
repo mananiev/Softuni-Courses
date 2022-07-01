c7c24534d5784f955bb17600ba02d75ce6cdf39f
mananiev <m.a.ananiev@gmail.com>
2022-07-01 17:09:35 +0300

    Added a few exercises

--- FundamentalsJava/src/ObjectsAndClasses/_01_RandomizeWords.java
+++ FundamentalsJava/src/ObjectsAndClasses/_01_RandomizeWords.java
@@ -1,9 +1,27 @@
 package ObjectsAndClasses;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class _01_RandomizeWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] wordArr = scanner.nextLine().split( " ");
+
+        Random random = new Random();
+
+        for (int i = 0; i < wordArr.length; i++) {
+
+            int x = random.nextInt(wordArr.length);
+            int y = random.nextInt(wordArr.length);
+
+            String temp = wordArr[x];
+            wordArr[x] = wordArr[y];
+            wordArr[y] = temp;
+
+        }
+
+        System.out.println(String.join(System.lineSeparator(), wordArr));
     }
 }
--- FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
+++ FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class _04_Songs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        
     }
 }
