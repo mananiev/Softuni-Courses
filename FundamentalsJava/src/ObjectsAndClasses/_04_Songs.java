16355fff6cfb3165df0cae14ba3d534c227f4678
mananiev <m.a.ananiev@gmail.com>
2022-07-05 16:58:27 +0300

    added exercise

--- FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
+++ FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
@@ -1,11 +1,58 @@
 package ObjectsAndClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class _04_Songs {
+
+    static class Song {
+        String typeList;
+        String name;
+        String time;
+
+        public void setTypeList(String typeList) {
+            this.typeList = typeList;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Song> songsList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            String[] data = scanner.nextLine().split("_");
+
+            String type = data[0];
+            String name = data[1];
+            String time = data[2];
+
+            Song currentSong = new Song();
+            currentSong.setTypeList(type);
+            currentSong.setName(name);
+            currentSong.setTime(time);
+
+            songsList.add(currentSong);
+
+
+        }
+
+        String command  = scanner.nextLine();
+
+        if (Objects.equals(command, "all")){
+
+        }
     }
 }
