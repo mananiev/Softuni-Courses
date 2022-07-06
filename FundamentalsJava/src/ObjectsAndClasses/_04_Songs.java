078ce25b4e19f07be6b97d06ec7a536efdb2ec05
mananiev <m.a.ananiev@gmail.com>
2022-07-06 17:45:37 +0300

    added exercise

--- FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
+++ FundamentalsJava/src/ObjectsAndClasses/_04_Songs.java
@@ -12,47 +12,71 @@ public class _04_Songs {
         String name;
         String time;
 
-        public void setTypeList(String typeList) {
-            this.typeList = typeList;
-        }
-
-        public void setName(String name) {
+        Song (String type, String name, String time) {
+            this.typeList = type;
             this.name = name;
-        }
-
-        public void setTime(String time) {
             this.time = time;
         }
+
+//        public void setTypeList(String typeList) {
+//            this.typeList = typeList;
+//        }
+
+        public String getTypeList() {
+            return this.typeList;
+        }
+
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+
+        public String getName() {
+            return this.name;
+        }
+
+//        public void setTime(String time) {
+//            this.time = time;
+//        }
+
+        public String getTime() {
+            return this.time;
+        }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
         List<Song> songsList = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
-
             String[] data = scanner.nextLine().split("_");
 
             String type = data[0];
             String name = data[1];
             String time = data[2];
 
-            Song currentSong = new Song();
-            currentSong.setTypeList(type);
-            currentSong.setName(name);
-            currentSong.setTime(time);
+            Song currentSong = new Song(type, name, time);
+
+//            currentSong.setTypeList(type);
+//            currentSong.setName(name);
+//            currentSong.setTime(time);
 
             songsList.add(currentSong);
-
-
         }
 
-        String command  = scanner.nextLine();
+        String command = scanner.nextLine();//all typeList
 
-        if (Objects.equals(command, "all")){
-
+        if (command.equals("all")) {
+            for (Song item : songsList) {
+                System.out.println(item.getName());
+            }
+        } else {
+            for (Song item : songsList) {
+                if (item.getTypeList().equals(command)) {
+                    System.out.println(item.getName());
+                }
+            }
         }
     }
 }
--- FundamentalsJava/src/ObjectsAndClasses/_05_Students.java
+++ FundamentalsJava/src/ObjectsAndClasses/_05_Students.java
@@ -1,9 +1,70 @@
 package ObjectsAndClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class _05_Students {
+    static class Student {
+        String firstName;
+        String lastName;
+        String age;
+        String town;
+
+        public Student(String firstName, String lastName, String age, String town){
+
+            this.firstName= firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.town= town;
+
+
+        }
+
+        public String getTown() {
+            return this.town;
+        }
+
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        public String getAge() {
+            return this.age;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Student> studentsList = new ArrayList<>();
+        String input = scanner.nextLine();
+
+        while (!Objects.equals(input, "end")) {
+            String[] dataArr = input.split(" ");
+
+            String firstName = dataArr[0];
+            String lastName = dataArr[1];
+            String age = dataArr[2];
+            String town = dataArr[3];
+
+            Student student = new Student(firstName, lastName, age, town);
+
+            studentsList.add(student);
+
+            input = scanner.nextLine();
+        }
+
+        String searchTown = scanner.nextLine();
+
+        for ( Student s : studentsList){
+            if (s.getTown().equals(searchTown)){
+                System.out.printf("%s %s is %s years old%n", s.getFirstName(), s.getLastName(), s.getAge());
+            }
+        }
     }
 }
